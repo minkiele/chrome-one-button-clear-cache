37bbc538f22334009aadd4a1f946064d89da7da6
Michele Montagner <michelasso@gmail.com>
2016-01-17 11:41:15 +0100

    Added jquery for some simple interaction

--- popup.js
+++ popup.js
@@ -1,13 +1,20 @@
-(function(ch){
-  ch.browsingData.removeCache({
-    since: 0,
-    originTypes: {
-      unprotectedWeb: true
-    }
-  }, function () {
-    console.log('Done clearing cache');
-    //Show the message to the world
-    document.getElementById('#cache-cleared').className = '';
+(function(ch, $){
+  var timeoutId = 0;
+  $('#clear-cache').on('click', function () {
+
+    clearTimeout(timeoutId);
+    $('#clear-cache-msg').addClass('hidden');
+
+    ch.browsingData.removeCache({
+      since: 0,
+      originTypes: {
+        unprotectedWeb: true
+      }
+    }, function () {
+      timeoutId = setTimeout(function () {
+        $('#clear-cache-msg').removeClass('hidden');
+      }, 5000);
+    });
   });
-}(chrome));
+}(chrome, jQuery));
 
